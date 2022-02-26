54ecdbdbc56a61153217eeaff68d7f0d41944e8a
Varun Vaishnav <varun.vaishnav@einfochips.com>
2022-02-26 10:09:42 +0530

    Switch from vlc to audacious

--- Group-8.sh
+++ Group-8.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#Script to scan storage for music and create a playlist and play it using mplayer/vlc
+#Script to scan storage for music and create a playlist and play it using mplayer/audacious
 #Shift 1, Track 2
 
 #Group -8: Varun Vaishnav, Vandit Prajapati, Vivek Vagheswari, Ved Bhonsle, Yash Jain, Kartheek Ikkurthi
 #Shaswat Shah, Shivam Pandey
 
 
-read -p "Enter 1 if to install/play using vlc media player or 2 to use mplayer" player
+read -p "Enter 1 if to install/play using audacious or 2 to use mplayer" player
 
 function scan() {
 #Scan /home and /media for mp3s and output it to playlist.m3u, and redirect errors to /dev/null
-find /home /media -iname '*.mp3' > playlist.m3u 2>/dev/null;
+find /home /media -iname '*.mp3' -print > playlist.m3u 2>/dev/null;
 }
 
 function installplayer() {
@@ -19,22 +19,23 @@ function installplayer() {
 	if [ ! -f /usr/bin/"$player" ];
 	then
 		echo "$player isn't installed, installing..";
+		sudo apt-get update;
 		sudo apt install "$player";
 	fi;
 }
 
 case $player in
 1)
-	#Assign player as vlc and call installplayer
-	player=vlc;
+	#Assign player as audacious and call installplayer
+	player=audacious;
 	installplayer;
 
-	echo "VLC found!, scanning storage for mp3s";
+	echo "Audacious found!, scanning storage for mp3s";
 
 	scan;
 
-	#Play the playlist using VLC
-	vlc playlist.m3u;
+	#Play the playlist using audacious
+	audacious --play playlist.m3u;
 	exit 0;
 	;;
 2)
